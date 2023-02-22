b1976fa7be5cc6bb1f3f37ab0e2d3b5e70211252
Margarita1209 <margarita.levil29@gmail.com>
2023-02-22 00:54:04 -0300

    otra parte del proyecto

---
+++ Relacion_MOD_EST.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConBase
+{
+    public partial class Relacion_MOD_EST : Form
+    {
+        public Relacion_MOD_EST()
+        {
+            InitializeComponent();
+            CargarCombo();
+            CargarComboModulo();
+        }
+        public void CargarCombo()
+        {
+            Estudiante es = new Estudiante();
+            try
+            {
+                cmbEstudiante.DataSource = es.CargaComboEstudiante();
+                cmbEstudiante.ValueMember = "EST_ID";
+                cmbEstudiante.DisplayMember = "EST_NOMBRE";
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void CargarComboModulo()
+        {
+            Estudiante es = new Estudiante();
+            try
+            {
+                cmbModulo.DataSource = es.CargaComboModulo();
+                cmbModulo.ValueMember = "MOD_ID";
+                cmbModulo.DisplayMember = "MOD_NOMBRE";
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Estudiante es = new Estudiante();
+            string MensajeResutado = null;
+            try
+            {
+                MensajeResutado = es.InsertaEstudianteModulo(Convert.ToInt32(cmbModulo.SelectedValue.ToString()), Convert.ToInt32(cmbEstudiante.SelectedValue.ToString()));
+
+                MessageBox.Show(MensajeResutado);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
